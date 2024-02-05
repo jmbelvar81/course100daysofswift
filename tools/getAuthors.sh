b11322fe501f797509ee431b2b2040e828c697b4
Chemabj <jmbelvar81@outlook.com>
2024-02-05 12:41:30 +0100

    Update getAuthors.sh
    Non threshold defined, fixing error

--- tools/getAuthors.sh
+++ tools/getAuthors.sh
@@ -13,4 +13,4 @@
 #   1) The filepath where storage the result
 #
 
-git log --format="%aN" | sort | uniq -c | sort -rn | awk '$1>=$THRESHOLD {$1=""; print $0}' | cut -d" " -f2- > $1
+git log --format="%aN" | uniq -c | sort -rn | awk '{print $2}' > $1
